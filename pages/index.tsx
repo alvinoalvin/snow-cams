58e3c7af1c44db0f4af0e54d49220003f8424ddb
Alvin Ng <alvinoalvin123@gmail.com>
2024-11-21 14:06:25 -0800

    adding firebase config

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,12 @@ export default function IndexPage() {
   return (
     <Group mt={50} justify="center">
       <Button size="xl">Welcome to Mantine!</Button>
+      <iframe
+        className="aspect-video h-auto w-full"
+        src="https://www.skaping.com/cypress-mountain/mount-strachan"
+        loading="lazy"
+        title="Sky Chair Camera"
+      ></iframe>
     </Group>
   );
 }
