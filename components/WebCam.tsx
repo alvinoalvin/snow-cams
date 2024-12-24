ea2142d91349bcb0dcfb3b42b252fc2b6d6795b7
Alvin Ng <alvinoalvin123@gmail.com>
2024-12-23 19:35:12 -0800

    [sc-18] proof of concept iframes

--- components/WebCam.tsx
+++ components/WebCam.tsx
@@ -4,23 +4,24 @@ interface WebCamProps extends IframeHTMLAttributes<HTMLIFrameElement> {
   type: "iframe" | "image";
   title: string;
   mountain: string;
+  src: string;
 }
 
 export const WebCam = (props: WebCamProps) => {
   return (
     <Card shadow="md" padding="sm" radius="md" withBorder w="30em" h={"30em"}>
       {props.type === "iframe" ? (
-        <iframe height="100%" {...props} />
+        <iframe
+          height="100%"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          {...props}
+        />
       ) : (
-        <Image src={props.src} fit={"fill"} radius="md" />
+        <Image src={props.src} fit={"fill"} radius="md" alt={props.title} />
       )}
-      <Card.Section  ta={"center"}>
-        <Title order={5}>
-        {props.mountain}
-        </Title>
-        <Title order={3}>
-        {props.title}
-        </Title>
+      <Card.Section ta={"center"}>
+        <Title order={5}>{props.mountain}</Title>
+        <Title order={3}>{props.title}</Title>
       </Card.Section>
     </Card>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,40 @@
+import { useEffect } from "react";
 import { WebCam } from "../components/WebCam";
+import { PrismaClient } from "@prisma/client";
+import { Grid } from "@mantine/core";
 
-export default function IndexPage() {
-  return (
-    <div>
-      <WebCam
-        className="aspect-video h-auto w-full"
-        src="https://www.skaping.com/cypress-mountain/mount-strachan"
-        loading="lazy"
-        title="Sky Chair Camera"
-        mountain="Cypress"
-        type="iframe"
-      />
-      <iframe
-        src="https://relay.ozolio.com//pub.api?cmd=explore&amp;oid=CID_YBLM00000045&amp;channel=0"
-        title="Grouse Plaza"
-      />
-      <iframe
-        src="https://www.youtube.com/embed/yQdKq9NuEa0"
-        title="Stoke-O-Meter Snowcam | Mt Seymour Ski Area, Vancouver, BC"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      />
-      <iframe
-        src="https://www.youtube.com/embed/vLawo-FrBKk"
-        title="Mystery Peak Webcam | Mt Seymour Ski Area, Vancouver, BC"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      />
-      <iframe
-        id="webcam_brownrice__player_1"
-        className="webcam_brownrice__player"
-        src="//player.brownrice.com/embed/whistlerroundhouse"
-        referrerPolicy="origin"
-        title="Roundhouse Lodge, Whistler Mountain"
-        allowFullScreen
-      />
-
-      <iframe
-        id="webcam_brownrice__player_3"
-        className="webcam_brownrice__player"
-        src="//player.brownrice.com/embed/whistlerblackcomb"
-        referrerPolicy="origin"
-        loading="lazy"
-        title="Rendezvous Lodge, Blackcomb Mountain"
-        allowFullScreen
-      />
-      <iframe
-        id="webcam_brownrice__player_6"
-        className="webcam_brownrice__player"
-        src="//player.brownrice.com/embed/whistlervillagefitz"
-        title="Whistler Village"
-        allowFullScreen
-      />
-      <iframe
-        id="webcam_brownrice__player_5"
-        className="webcam_brownrice__player"
-        src="//player.brownrice.com/embed/whistlerpeak"
-        allowFullScreen
-        title="Whistler Peak"
-      />
-      <iframe
-        id="webcam_brownrice__player_7"
-        className="webcam_brownrice__player"
-        src="//player.brownrice.com/embed/whistlercreekside"
-        title="Whistler Creekside"
-        allowFullScreen
-      />
-      <iframe
-        id="webcam_brownrice__player_8"
-        className="webcam_brownrice__player"
-        src="//player.brownrice.com/embed/whistlerglacier"
-        title="Whistler Glacier"
-        allowFullScreen
-      />
-      <iframe
-        id="webcam_brownrice__player_9"
-        className="webcam_brownrice__player"
-        src="//player.brownrice.com/embed/whistlersnowstack"
-        title="Snow Ruler"
-        allowFullScreen
-      />
-    </div>
+interface ICam {
+  id: number;
+  title: string;
+  mountain: string;
+  type: "iframe" | "image";
+  link: string;
+}
+export default function IndexPage({ cams }: any) {
+  return cams.length <= 0 ? (
+    <>Loading...</>
+  ) : (
+    <Grid gutter={"md"}>
+      {cams.map((cam: ICam) => {
+        return (
+          <Grid.Col key={cam.id} span={4}>
+            <WebCam
+              className="aspect-video h-auto w-full"
+              src={cam.link}
+              loading="lazy"
+              title={cam.title}
+              mountain={cam.mountain}
+              type={cam.type}
+            />
+          </Grid.Col>
+        );
+      })}
+    </Grid>
   );
+}
+
+export async function getServerSideProps() {
+  const prisma = new PrismaClient();
+  const cams = await prisma.cams.findMany();
+  return { props: { cams } };
 }
