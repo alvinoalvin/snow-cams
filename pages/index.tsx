076ae8b12d8167066cfc5a78d385e9b3050a6dd1
Alvin <alvinoalvin123@gmail.com>
2025-02-26 13:29:26 -0800

    [sc-40] search functionality UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,68 @@
 import { WebCam } from "../components/WebCam";
 import { PrismaClient } from "@prisma/client";
-import { Grid } from "@mantine/core";
-import { Pool, neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import dotenv from 'dotenv'
-import ws from 'ws'
+import { Grid, TextInput } from "@mantine/core";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import dotenv from "dotenv";
+import ws from "ws";
+import { ChangeEvent, useState } from "react";
 
-dotenv.config()
-neonConfig.webSocketConstructor = ws
-const connectionString = `${process.env.DATABASE_URL}`
+dotenv.config();
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const pool = new Pool({ connectionString })
-const adapter = new PrismaNeon(pool)
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 
 interface ICam {
-  id: number;
+  link: string;
   title: string;
+  id: number;
   mountain: string;
   type: "iframe" | "image";
-  link: string;
 }
 export default function IndexPage({ cams }: any) {
   /* Test use MST  */
-  // const { webcamStore } = useMst();
-  // useEffect(() => {
-  //   webcamStore.getInitialCams();
-  // }, []);
-  // cams = webcamStore.webcams;
+  const [filteredCams, setFilteredCams] = useState(cams);
 
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilteredCams(
+      cams.filter((cam: ICam) =>
+        cam.mountain.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    );
+  };
   return cams.length <= 0 ? (
     <>Loading...</>
   ) : (
-    <Grid
-      gutter={{ base: 5, xs: "md", md: "xl", xl: 50 }}
-      w={{ xs: "80%", md: "100%", lg: "85%", xl: "80%" }}
-      m="auto"
-    >
-      {cams.map((cam: ICam) => {
-        return (
-          <Grid.Col key={cam.id} span={{ sm: 12, md: 6, xl: 4 }}>
-            <WebCam
-              className="aspect-video h-auto w-full"
-              src={cam.link}
-              title={cam.title}
-              mountain={cam.mountain}
-              type={cam.type}
-            />
-          </Grid.Col>
-        );
-      })}
-    </Grid>
+    <>
+      <Grid
+        gutter={{ base: 5, xs: "md", md: "xl", xl: 50 }}
+        w={{ xs: "95%", md: "90%", lg: "90%", xl: "95%" }}
+        m="auto"
+      >
+        <Grid.Col span={12}>
+          <TextInput
+            placeholder="Search..."
+            w={"12.5rem"}
+            onChange={(e) => handleSearch(e)}
+          />
+        </Grid.Col>
+        {filteredCams.map((cam: ICam) => {
+          return (
+            <Grid.Col key={cam.id} span={"auto"}>
+              <WebCam
+                className="aspect-video h-auto w-full"
+                src={cam.link}
+                title={cam.title}
+                mountain={cam.mountain}
+                type={cam.type}
+              />
+            </Grid.Col>
+          );
+        })}
+      </Grid>
+    </>
   );
 }
 
